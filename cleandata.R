cd3d62877cbb8c03c7c3abfcdec184e5ed6fa132
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-31 12:09:34 -0400

    Saving cleandata.R

--- cleandata.R
+++ cleandata.R
@@ -3,18 +3,20 @@ project= read.csv("SatisfactionSurvey.csv")
 str(project)
 
 project$Flight.date <- as.Date(project$Flight.date, "%m/%d/%Y")
+
 project$Flight.date <- format(project$Flight.date, "%m-%d-%y")
 
 project$Destination.City=gsub(",","", project$Destination.City)
 
 
 project$Orgin.City=gsub(",","", project$Orgin.City)
-View(project)
+
 
 project$Flight.date=as.Date(project$Flight.date, "%m/%d/%y")
-View(project)
+
 project$Flight.date=gsub("-", "/", project$Flight.date, fixed=T )
-View(project)
+
+project
 
 sum(is.na(project))
 summary(project)

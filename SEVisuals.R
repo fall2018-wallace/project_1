85a1822d772a1a1b00680dfa29f682dea28de327
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-09 20:06:12 -0500

    Saving SEVisuals.R

--- SEVisuals.R
+++ SEVisuals.R
@@ -46,19 +46,19 @@ FlightPerSE <- ggplot(project_SE,aes(x=project_SE$No.of.Flights.p.a.,y=project_S
   stat_summary(aes(y =project_SE$Satisfaction,group=1), fun.y=mean, colour="red", geom="point",group=1)
 
 #12.Satisfaction vs.No. of Flights per airline for all other airlines
-ggplot(project_other,aes(x=project_other$No.of.Flights.p.a.,y=project_other$Satisfaction))+geom_count()+
+FlightPerOther <- ggplot(project_other,aes(x=project_other$No.of.Flights.p.a.,y=project_other$Satisfaction))+geom_count()+
   stat_summary(aes(y =project_other$Satisfaction,group=1), fun.y=mean, colour="red", geom="point",group=1)
 
 #13.Satisfaction vs.class for SE
-ggplot(project_SE,aes(x=project_SE$Class,y=project_SE$Satisfaction))+geom_count()+
+ClassSE <- ggplot(project_SE,aes(x=project_SE$Class,y=project_SE$Satisfaction))+geom_count()+
   stat_summary(aes(y =project_SE$Satisfaction,group=1), fun.y=mean, colour="red", geom="point",group=1)
 
 #14.Satisfaction vs. class for all other airlines
-ggplot(project_other,aes(x=project_other$Class,y=project_other$Satisfaction))+geom_count()+
+ClassOther <- ggplot(project_other,aes(x=project_other$Class,y=project_other$Satisfaction))+geom_count()+
   stat_summary(aes(y =project_other$Satisfaction,group=1), fun.y=mean, colour="red", geom="point",group=1)
 
 #15.Satisfaction vs. Price Sensitivity for SE
-ggplot(project_SE,aes(x=project_SE$Price.Sensitivity,y=project_SE$Satisfaction))+geom_count()+
+PriceSenSE <- ggplot(project_SE,aes(x=project_SE$Price.Sensitivity,y=project_SE$Satisfaction))+geom_count()+
   stat_summary(aes(y =project_SE$Satisfaction,group=1), fun.y=mean, colour="red", geom="point",group=1)
 
 #16.Satisfaction vs.No. of Flights per airline for oall other airlines

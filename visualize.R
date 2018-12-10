1384db39c2f8d373c3346d076a5e244adf6f67a8
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-09 19:06:43 -0500

    Saving visualize.R

--- visualize.R
+++ visualize.R
@@ -1,28 +1,84 @@
 
 options(scipen=999)
+
 library(ggplot2)
 
-hist_sat<-ggplot(project, aes(x=Satisfaction)) + geom_histogram(bins=50,color="black", fill="black")
-hist_sat
+#1.Customer Satisfaction Based on Airline
+sat=table(project$Satisfaction, project$Airline.Name)
+sat
+airlinenamesat=barplot(sat, main="How many customers are Satisfied with an Airline?", cex.names = 0.45,
+                       col=c("darkblue","red", "yellow", "orange", "pink"),ylab="Number of Customers",
+                       las=2, legend=rownames("counts2"), args.legend = list(x = "top", ncol=5, inset=c(0, 0), cex=.4))
 
-travel_sat_bar=ggplot(project, aes(x=Type.of.Travel, y= Satisfaction)) + geom_col(aes(fill=Satisfaction))
-travel_sat_bar
 
-travel_sat_box=ggplot(project,aes(x=Type.of.Travel,y=Satisfaction))+geom_boxplot(aes(fill=Type.of.Travel))
-travel_sat_bar
+#2. PriceSensitivity with respect to AirlineName
+Count1=nrow(project[which(project$Price.Sensitivity == "1"),])
+Count2=nrow(project[which(project$Price.Sensitivity == "2"),])
+Count3=nrow(project[which(project$Price.Sensitivity == "3"),])
+Count4=nrow(project[which(project$Price.Sensitivity == "4"),])
+Count5=nrow(project[which(project$Price.Sensitivity == "5"),])
+NewData1=table(project$Price.Sensitivity, project$Airline.Name)
+NewData1
+NewData1=data.frame(NewData1)
+colnames(NewData1)=c("PriceSensitivity","AirlineName","Frequency")
+View(NewData1)
+x=NewData1
+x$Satisfaction[which(x$PriceSensitivity=="1")]=x$Satisfaction[which(x$PriceSensitivity=="1")]/Count1*100
+x$Satisfaction[which(x$PriceSensitivity=="2")]=x$Satisfaction[which(x$PriceSensitivity=="2")]/Count2*100
+x$Satisfaction[which(x$PriceSensitivity=="3")]=x$Satisfaction[which(x$PriceSensitivity=="3")]/Count3*100
+x$Satisfaction[which(x$PriceSensitivity=="4")]=x$Satisfaction[which(x$PriceSensitivity=="4")]/Count4*100
+x$Satisfaction[which(x$PriceSensitivity=="5")]=x$Satisfaction[which(x$PriceSensitivity=="5")]/Count5*100
+NewData1=x
+ggplot(NewData1,aes(x = PriceSensitivity, fill=AirlineName, y = Frequency))+geom_col(position = "dodge")
 
-airline_sat_bar=ggplot(project, aes(x=Airline.Status, y= Satisfaction)) + geom_col(aes(fill=Satisfaction))
-airline_sat_bar
+#3. Satisfaction vs Age 
+ggplot(project,aes(x=project$Age,y=project$Satisfaction))+geom_count()+
+  stat_summary(aes(y =project$Satisfaction,group=1), fun.y=mean, colour="red", geom="point",group=1)
 
-airline_sat_box=ggplot(project,aes(x=Airline.Status,y=Satisfaction))+geom_boxplot()
-airline_sat_box
+#4.Class of Travel with respect to Type of travel
+Count6=nrow(project[which(project$Type.of.Travel == "Personal Travel"),])
+Count7=nrow(project[which(project$Type.of.Travel == "Business travel"),])
+Count8=nrow(project[which(project$Type.of.Travel == "Mileage tickets"),])
+NewData2=table(project$Type.of.Travel, project$Class)
+NewData2=data.frame(NewData2)
+colnames(NewData2)=c("TypeofTravel","ClassofTravel","Frequency")
+View(NewData2)
+x=NewData2
+x$Satisfaction[which(x$Type.of.Travel=="Personal Travel")]=x$Satisfaction[which(x$Type.of.Travel=="Personal Travel")]/Count1*100
+x$Satisfaction[which(x$Type.of.Travel=="Business travel")]=x$Satisfaction[which(x$Type.of.Travel=="Business Travel")]/Count2*100
+x$Satisfaction[which(x$Type.of.Travel=="Mileage tickets")]=x$Satisfaction[which(x$Type.of.Travel=="Mileage Travel")]/Count3*100
+NewData2=x
+ggplot(NewData2,aes(x = TypeofTravel, fill=ClassofTravel, y = Frequency))+geom_col(position = "dodge")
 
-price_sat_bar=ggplot(project, aes(x=Price.Sensitivity, y= Satisfaction)) + geom_col(aes(fill=Satisfaction))
-price_sat_bar
+#5.Satisfaction vs. Gender
+GenderSatisfaction=ggplot(project, aes(Gender, Satisfaction))+geom_boxplot(aes(fill = Gender))+labs(x="Gender", y="Satisfaction")
+GenderSatisfaction=GenderSatisfaction + stat_summary(fun.y=mean, colour="darkred", geom="point", 
+                                                     shape=18, size=3,show_guide = FALSE)
+GenderSatisfaction
 
-price_sat_box=ggplot(project,aes(x=Price.Sensitivity,y=Satisfaction))+geom_boxplot(aes(fill=Satisfaction, group= Price.Sensitivity))
-price_sat_box
+#6.Satisfaction vs. Type of Travel
+TypeoftravelSat=ggplot(project, aes(x=Type.of.Travel, y=Satisfaction, colour= "red")) + geom_boxplot(fill="yellow")
+TypeoftravelSat=TypeoftravelSat + stat_summary(fun.y=mean, colour="darkred", geom="point", 
+                                               shape=18, size=3,show_guide = FALSE)
+TypeoftravelSat
 
-airline_sat_bar=ggplot(project, aes(x=Airline.Name, y= Satisfaction)) + geom_col(aes(fill=Satisfaction))+ theme(axis.text.x=element_text(angle=90, hjust=1))
-airline_sat_bar
+#7.Satisfaction with respect to airline status
+AirlineStatSatisfaction=ggplot(project, aes(x=Airline.Status, y= Satisfaction)) + geom_col(aes(fill=Satisfaction))
+AirlineStatSatisfaction=AirlineStatSatisfaction +xlab("Airline Status") + ylab("Count")
+AirlineStatSatisfaction
+
+#8.Satisfaction with respect to price Sensitivity
+PriceSensitivitySatisfaction=ggplot(project, aes(x=Price.Sensitivity, y= Satisfaction)) + geom_col(aes(fill=Satisfaction))
+PriceSensitivitySatisfaction=PriceSensitivitySatisfaction + xlab("Price Sensitivity") + ylab("Count")
+PriceSensitivitySatisfaction
+
+#9. Satisfaction with respect to Type of Travel
+TypeofTravelSatisfaction=ggplot(project, aes(x=Type.of.Travel, y= Satisfaction)) + geom_col(aes(fill=Satisfaction))
+TypeofTravelSatisfaction=TypeofTravelSatisfaction + xlab("Type of Travel") + ylab("Count")
+TypeofTravelSatisfaction
+
+#10.Satisfaction vs. Shopping Amount
+ShoppingAmountSatisfaction=ggplot(project, aes(x=project$Shopping.Amount.at.Airport,y=project$Satisfaction))+geom_count()
+ShoppingAmountSatisfaction=ShoppingAmountSatisfaction + xlab("Shopping Amount") + ylab("Satisfaction")
+ShoppingAmountSatisfaction
 
